34bd1a6af17ca599505a8bdee287299a663d0223
Mateusz Czernik <Mateusz.Czernik@microsoft.wsei.edu.pl>
2022-08-31 15:38:35 +0200

    Glownywidok

---
+++ PROJECT/MVVM/ModelWidoku/ModelGłównegoWidoku.cs
@@ -0,0 +1,17 @@
+﻿using PROJECT.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT.MVVM.ModelWidoku
+{
+    class MGW : ObservableObject
+    {
+        public MGW() 
+        { 
+
+        }
+    }
+}
