d449fcea14330d1026a7e2b1cbf26006d754c2b5
Mateusz Czernik <Mateusz.Czernik@microsoft.wsei.edu.pl>
2022-09-03 14:57:43 +0200

    x

--- PROJECT/MVVM/View/HomeView.xaml.cs
+++ PROJECT/MVVM/View/HomeView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PROJECT.MVVM.Widok
+namespace PROJECT.MVVM.View
 {
     /// <summary>
     /// Logika interakcji dla klasy HomeView.xaml
--- PROJECT/MVVM/ViewModel/MainViewModel.cs
+++ PROJECT/MVVM/ViewModel/MainViewModel.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PROJECT.MVVM.ModelWidoku
+namespace PROJECT.MVVM.MainViewModel
 {
     class MGW : ObservableObject
     {
