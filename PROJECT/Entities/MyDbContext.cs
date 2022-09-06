5e1419bd6fd32c5ec11be5233cc7ae7326f994e5
Mateusz Czernik <Mateusz.Czernik@microsoft.wsei.edu.pl>
2022-09-06 20:25:49 +0200

    baza

--- PROJECT/Entities/MyDbContext.cs
+++ PROJECT/Entities/MyDbContext.cs
@@ -9,6 +9,12 @@ namespace PROJECT.Entities
 {
     class MyDbContext : DbContext
     {
+        string baza = "Data Source=DESKTOP-1D66II7;Initial Catalog=PROJECT;Integrated Security=True";
 
+        public DbSet<Karnety> Karnety { get; set; }
+        public DbSet<Klienci> Klienci { get; set; }
+        public DbSet<Rabaty> Rabaty { get; set; }
+        public DbSet<Trenerzy> Trenerzy { get; set; }
+        public DbSet<Typy_Karnetu> Typy_Karnetu { get; set; }
     }
 }
