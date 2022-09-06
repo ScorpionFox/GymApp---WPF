fc0503ca8284713b58280ecd368fb8e592ebe058
Mateusz Czernik <Mateusz.Czernik@microsoft.wsei.edu.pl>
2022-09-06 21:47:13 +0200

    x

--- PROJECT/MVVM/View/HomeView.xaml.cs
+++ PROJECT/MVVM/View/HomeView.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PROJECT.Entities;
+using System.Diagnostics;
 
 namespace PROJECT.MVVM.View
 {
@@ -23,6 +25,26 @@ namespace PROJECT.MVVM.View
         public HomeView()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine(Pesel.Text);
+            //int P = int.Parse(Pesel.Text.ToString());
+            test(207);
+        }
+
+        private void test(int Pesel)
+        {
+            using (var db = new MyDbContext())
+            {
+                var query = (from T in db.Klienci
+                             join K in db.Karnety on T.Pesel equals K.ID_Klient
+                             where T.Pesel == Pesel
+                             select new { Name = T.Imie, Surname = T.Nazwisko, Date = K.Data_waznosci }).ToList();
+                  DataGrid.ItemsSource = query;
+
+            }
         }
     }
 }
